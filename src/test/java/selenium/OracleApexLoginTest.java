d661d7ef8e74c185a3fb76312b319d4a19b46937
fistle <davielang@gmail.com>
2021-10-01 10:45:23 +0100

    Update OracleApexLoginTest.java

--- src/test/java/selenium/OracleApexLoginTest.java
+++ src/test/java/selenium/OracleApexLoginTest.java
@@ -7,41 +7,37 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class OracleApexLoginTest {	
-	
+public class OracleApexLoginTest {		
 	WebDriver driver = null;
-	
-	
+		
 	public OracleApexLoginTest()
 	{
-		setUpDriver();
+	    setUpDriver();
 	}	
-
 	
 	@Test
-	public void login2() {			
-		driver.get("https://kjwvivmv0n5reuj-apexkqor.adb.uk-london-1.oraclecloudapps.com/ords/r/test1/selenium-test-app/home");
-		driver.findElement(By.id("P9999_USERNAME")).click();
+	public void loginToApexAppAndEnsurePageContainsText() {			
+	    driver.get("https://kjwvivmv0n5reuj-apexkqor.adb.uk-london-1.oraclecloudapps.com/ords/r/test1/selenium-test-app/home");
+	    driver.findElement(By.id("P9999_USERNAME")).click();
 	    driver.findElement(By.id("P9999_USERNAME")).sendKeys("SELENIUM_TEST");
 
-	    //driver.findElement(By.cssSelector(".t-Login-header")).click();
 	    driver.findElement(By.id("P9999_PASSWORD")).click();
 	    driver.findElement(By.id("P9999_PASSWORD")).sendKeys("Abc12!abc@12");
 	    
 	    driver.findElement(By.xpath("//button[contains(.,\'Sign In\')]")).click();
-		Assert.assertTrue(driver.getPageSource().contains("Selenium test app XX")); 
+	    Assert.assertTrue(driver.getPageSource().contains("Selenium test app XX")); 
 		
-		driver.quit();	        
+	    driver.quit();	        
 	}
 	
 	private void setUpDriver()
 	{
-WebDriverManager.chromedriver().setup();
- ChromeOptions options = new ChromeOptions();
- options.addArguments("--no-sandbox");
- options.addArguments("--disable-dev-shm-usage");
- options.addArguments("--headless");
- driver = new ChromeDriver(options);
+	    WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
+	    options.addArguments("--disable-dev-shm-usage");
+	    options.addArguments("--headless");
+	    driver = new ChromeDriver(options);
 	}
 }	
 
